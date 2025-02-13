0a4188cfbe8e5d3be7bf9921edef6b6c65dd5129
Victor H Coronado <victorhenriquescoronado@gmail.com>
2025-02-13 22:11:14 +0000

    Updated page.tsx

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
     <div className="relative flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: 'url(./bg-login.jpg)', filter: 'brightness(0.4)' }}></div>
       <div className="relative flex w-full max-w-sm flex-col gap-6">
-        
+        Teste
         <LoginForm />
       </div>
     </div>
