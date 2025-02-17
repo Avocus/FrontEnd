6bf1a7a3e03b1d10e20e9056b971a3548b99fd76
Avocuss <avocuss@gmail.com>
2025-02-17 11:36:21 -0300

    criação de tela de cadastro

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,12 +1,8 @@
-import { ModeToggle } from "@/components/ui/modeToggle";
 import Login from "../app/login/page";
 
 export default function Home() {
   return (
     <div>
-        <div style={{ position: "absolute", bottom: "15px", right: "15px", zIndex: 1000 }}>
-          <ModeToggle />
-        </div>
         <Login />
     </div>
   );
